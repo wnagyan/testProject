a914ea9619a3b06e79034619344916c3fa396998
zhoujh <1286184112@qq.com>
2022-08-18 10:38:42 +0800

    修改1

--- src/main/java/com/maycur/Main.java
+++ src/main/java/com/maycur/Main.java
@@ -6,9 +6,9 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-//        String s1 = "abc";
-//        String s2 = "abc";
-//        System.out.println(s1 == s2.intern());
+        String s1 = "abc";
+        String s2 = "abc";
+        System.out.println(s1 == s2.intern());
 
     }
 }
